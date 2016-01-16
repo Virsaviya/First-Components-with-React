b100986ccfd143a5e70eb654f99e2c68e3899a1c
Virsaviya <Virsaviya.Efraim@gmail.com>
2016-01-15 18:24:45 -0800

    heroku ready

---
+++ public/js/components/tableBody.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class TableBody extends React.Component {
+  render() {
+    let keys = Object.keys(this.props.data[0]);
+    let rows = this.props.data;
+    rows = rows.map( (row, idx) => {
+      let values = keys.map( (key, idx) => {
+        return <td key={idx}> {row[key]} </td>
+      });
+      return <tr key={idx}> {values} </tr>
+    });
+    return (
+      <tbody>
+        {rows}
+      </tbody>
+    );
+  }
+}
+
+export default TableBody;
